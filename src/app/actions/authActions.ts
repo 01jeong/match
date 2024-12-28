b317253cc8fb70fcef3a6b8e04de84d1ae129557
01jeong <{ID}+{username}@users.noreply.github.com>
2024-12-28 15:14:56 +0900

    end of section 5

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { signIn, signOut } from '@/auth';
+import { auth, signIn, signOut } from '@/auth';
 import { prisma } from '@/lib/prisma';
 import { LoginSchema } from '@/lib/schemas/loginSchema';
 import { registerSchema, RegisterSchema } from '@/lib/schemas/RegisterSchema';
@@ -9,33 +9,35 @@ import { User } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { AuthError } from 'next-auth';
 
-export async function signInUser(data: LoginSchema): Promise<ActionResult<string>> {
+export async function signInUser(
+  data: LoginSchema
+): Promise<ActionResult<string>> {
   try {
     const result = await signIn('credentials', {
       email: data.email,
       password: data.password,
-      redirect: false
-    })
-    console.log(result)
+      redirect: false,
+    });
+    console.log(result);
 
-    return {status: 'success', data: 'Logged in'}
+    return { status: 'success', data: 'Logged in' };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
-          return {status: 'error', error: 'Invalid credentials'}             
+          return { status: 'error', error: 'Invalid credentials' };
         default:
-          return {status: 'error', error: 'Something went wrong'};
+          return { status: 'error', error: 'Something went wrong' };
       }
     } else {
-      return {status: 'error', error: 'Something else went wrong'};
+      return { status: 'error', error: 'Something else went wrong' };
     }
   }
 }
 
 export async function signOutUser() {
-  await signOut({redirectTo: '/'})
+  await signOut({ redirectTo: '/' });
 }
 
 export async function registerUser(
@@ -77,4 +79,13 @@ export async function getUserByEmail(email: string) {
 }
 export async function getUserById(id: string) {
   return prisma.user.findUnique({ where: { id } });
+}
+
+export async function getAuthUserId() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error('Unauthorized');
+  
+  return userId;
 }
